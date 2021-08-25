e1ebbb0afeb1b2178b0e61a811210aebf1b3d9a5
JohnS93 <john.song8293@gmail.com>
2021-08-24 22:51:34 -0400

    acc Controler and service done

--- src/main/java/com/tresmigos/fullstackvideo/service/AccountService.java
+++ src/main/java/com/tresmigos/fullstackvideo/service/AccountService.java
@@ -1,11 +1,52 @@
 package com.tresmigos.fullstackvideo.service;
 
+import com.tresmigos.fullstackvideo.model.Account;
+import com.tresmigos.fullstackvideo.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AccountService {
 
+    private AccountRepository accountRepository;
 
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
 
+    public Account create(Account account){
+        return accountRepository.save(account);
+    }
+
+    public Account read(Long id){
+        return accountRepository.findById(id).orElse(null);
+    }
+    public List<Account> readAll(){
+        Iterable<Account> accountIterable = accountRepository.findAll();
+        List<Account> result = new ArrayList<>();
+        accountIterable.forEach(result :: add);
+        return  result;
+    }
+
+    public Account update(Long id, Account newAccountData){
+        Account accountInDb = read(id);
+        accountInDb.setId(newAccountData.getId());
+        accountInDb.setUsername(newAccountData.getUsername());
+        accountInDb.setPassword(newAccountData.getPassword());
+        return  accountRepository.save(accountInDb);
+    }
+
+    public Account delete(Long id){
+        Account accountInDb = read(id);
+        accountRepository.deleteById(id);
+        return accountInDb;
+    }
+
+    public Account delete(Account account){
+        return delete(account.getId());
+    }
 }
