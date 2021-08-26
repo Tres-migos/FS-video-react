886a173c59b794af27d5053a63d25462e812befa
Nathan <nathan@zipcodes-MacBook-Pro-9.local>
2021-08-26 17:53:14 -0400

    reformatted awsServiceClient

--- src/main/java/com/tresmigos/fullstackvideo/Main.java
+++ src/main/java/com/tresmigos/fullstackvideo/Main.java
@@ -5,7 +5,7 @@ import com.tresmigos.fullstackvideo.service.AwsServiceClient;
 public class Main {
     public static void main(String[] args) {
         AwsServiceClient awsServiceClient = new AwsServiceClient();
-        awsServiceClient.putInBucket("FirstAidFail.mp4","First Aid Fail - The Office");
+        //awsServiceClient.putInBucket("FirstAidFail.mp4","First Aid Fail - The Office");
         System.out.println(awsServiceClient.getObjUrl("NewRules-DuaLipa"));
     }
 
